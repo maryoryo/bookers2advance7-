1db7359cf16e396d8a9b53872957a9cdcab7dd56
maryoryo <liyomaxxde@gmail.com>
2021-06-03 13:12:23 +0000

    [Complete]応用課題第6回

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,26 +1,32 @@
 class BookCommentsController < ApplicationController
 
+  before_action :books_comments_params
+  
 
   def create
     @book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
     if @comment.save
-      redirect_to book_path(@book), notice: "You have created bookcomment successfully."
+      # redirect_to book_path(@book), notice: "You have created bookcomment successfully."
     else
       @book_comment = @comment
       @book_user = @book.user
-      render "books/show"
+      # render "books/show"
     end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    # redirect_to book_path(params[:book_id])
   end
 
 
 private
+
+  def books_comments_params
+     @book = Book.find(params[:book_id])
+  end
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
