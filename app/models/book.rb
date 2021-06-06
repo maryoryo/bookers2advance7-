1e8e30589cd67213e3cc1fc0af09531085786f9c
maryoryo <liyomaxxde@gmail.com>
2021-06-06 12:13:58 +0000

    [Complete]応用課題7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,6 +11,11 @@ class Book < ApplicationRecord
 		favorites.where(user_id: user.id).exists?
 	end
 	
+	scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } # 前週
+	
 	def self.search(search, word)
 		if search == "forward_match"
 			@book = Book.where("title LIKE?","#{word}%")
