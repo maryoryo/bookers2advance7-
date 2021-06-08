d855b221580cd47b06456f16a7ebae5201d33520
maryoryo <liyomaxxde@gmail.com>
2021-06-08 17:46:54 +0000

    [Complete]応用課題9d

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,8 +4,13 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+  validates :rate, presence: true
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
+  end
+  
+  def Book.search(search_word)
+    Book.where(['category LIKE ?', "#{search_word}"])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_112406) do
+ActiveRecord::Schema.define(version: 2021_06_08_161427) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_09_13_112406) do
     t.float "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "favorites", force: :cascade do |t|
